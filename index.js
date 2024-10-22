443ba8c09f947d135e80eb15a61aa20fc419b78a
Jossdel <josuedelgadil01@gmail.com>
2024-10-21 21:25:46 -0400

    cambios en el servidor

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hola Node")
+const express = require ("express")
+
+class Jugador {
+constructor(id){
+   this.id = id;
+}
+}
+
+const app = express();
+
+const jugadores = []; 
+
+app.get("/unirse", (req, res)=>{
+const id = `${Math.random()}`;
+
+const jugador = new Jugador (id)
+
+jugadores.push(jugador);
+
+    res.send(id)
+})
+
+app.listen(5000 , ()=>{
+    console.log("el servidor esta funcionando")
+});
